1260db74a57c4560f773d0d8e654fc9b2b6fabe1
qlenlen <1783619611@qq.com>
2024-08-10 17:52:50 +0800

    fix zip error

--- build.py
+++ build.py
@@ -70,7 +70,7 @@ elif os.name == "posix":
             continue
         shutil.rmtree(local + os.sep + "bin" + os.sep + "Linux" + os.sep + i)
 for i in os.listdir(local):
-    if i not in ["run", "run.exe", "bin", "LICENSE"]:
+    if i not in ["run", "run.exe", "bin", "LICENSE", "ksu-drivers"]:
         print(f"Removing {i}")
         if os.path.isdir(local + os.sep + i):
             try:
