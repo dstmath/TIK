6fd090fd5d9af154d1d24ebdaa1fbf0bbee13aa3
qlenlen <1783619611@qq.com>
2024-08-13 10:56:37 +0800

    update

--- build.py
+++ build.py
@@ -5,6 +5,28 @@ import zipfile
 
 import banner
 
+
+def zip_folder(folder_path: str, name: str):
+    abs_folder_path = os.path.abspath(folder_path)
+
+    local = os.getcwd()
+    zip_file_path = os.path.join(local, name)
+    archive = zipfile.ZipFile(zip_file_path, "w", zipfile.ZIP_DEFLATED)
+
+    for root, dirs, files in os.walk(abs_folder_path):
+        for file in files:
+            if file == name:
+                continue
+            file_path = os.path.join(root, file)
+            if ".git" in file_path:
+                continue
+            print(f"Adding: {file_path}")
+            archive.write(file_path, os.path.relpath(file_path, abs_folder_path))
+
+    archive.close()
+    print(f"Done!")
+
+
 ZIP_WHITELIST = ["run", "run.exe", "bin", "LICENSE"]
 
 print(f"\033[31m {banner.banner1} \033[0m")
@@ -20,33 +42,11 @@ else:
     name = "TIK-win.zip"
 
 
-def zip_folder(folder_path):
-    # 获取文件夹的绝对路径和文件夹名称
-    abs_folder_path = os.path.abspath(folder_path)
-
-    # 创建一个同名的zip文件
-    zip_file_path = os.path.join(local, name)
-    archive = zipfile.ZipFile(zip_file_path, "w", zipfile.ZIP_DEFLATED)
-
-    # 遍历文件夹中的所有文件和子文件夹
-    for root, dirs, files in os.walk(abs_folder_path):
-        for file in files:
-            if file == name:
-                continue
-            file_path = os.path.join(root, file)
-            if ".git" in file_path:
-                continue
-            print(f"Adding: {file_path}")
-            # 将文件添加到zip文件中
-            archive.write(file_path, os.path.relpath(file_path, abs_folder_path))
-
-    # 关闭zip文件
-    archive.close()
-    print(f"Done!")
-
-
 os.system("pyinstaller -F run.py --exclude-module=numpy -i icon.ico")
 
+
+TARGET_ARCH = ARCH if (ARCH := platform.machine()) else ""
+print(f"Target Arch: {TARGET_ARCH}")
 if os.name == "nt":
     if os.path.exists(local + os.sep + "dist" + os.sep + "run.exe"):
         shutil.move(local + os.sep + "dist" + os.sep + "run.exe", local)
@@ -62,9 +62,10 @@ elif os.name == "posix":
     if os.path.exists(local + os.sep + "bin" + os.sep + "Windows"):
         shutil.rmtree(local + os.sep + "bin" + os.sep + "Windows")
     for i in os.listdir(local + os.sep + "bin" + os.sep + "Linux"):
-        if i == platform.machine():
+        if i == TARGET_ARCH:
             continue
         shutil.rmtree(local + os.sep + "bin" + os.sep + "Linux" + os.sep + i)
+
 for i in os.listdir(local):
     if i not in ZIP_WHITELIST:
         print(f"Removing {i}")
@@ -80,10 +81,11 @@ for i in os.listdir(local):
                 print(e)
     else:
         print(i)
+
 if os.name == "posix":
     for root, dirs, files in os.walk(local, topdown=True):
         for i in files:
             print(f"Chmod {os.path.join(root, i)}")
             os.system(f"chmod a+x {os.path.join(root, i)}")
 
-zip_folder(".")
+zip_folder(".", name)
